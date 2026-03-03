858c7ba1c567d1aa88cf6cf4820cbdaecba1fa3f
LOPEZ2306 <juanmalopvel@gmail.com>
2026-03-03 18:50:20 -0500

    Solucion al segundo ejercicio propuesto para preparacion de parcial

---
+++ Propuesto2/Empleado.java
@@ -0,0 +1,34 @@
+public class Empleado {
+    private String nombre;
+    private double salario;
+    private String departamento;
+
+    // Constructor vacío
+    public Empleado() {
+    }
+
+    // Getters y Setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+}
